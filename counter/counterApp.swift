cb3c379b3db6eb03e3fe4b0e41782cccc53a9ecd
M Montserrat Gomez <gome0145@algonquinlive.com>
2023-11-06 17:37:22 -0500

    Initial Commit

---
+++ counter/counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterApp.swift
+//  counter
+//
+//  Created by Montserrat Gomez on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
